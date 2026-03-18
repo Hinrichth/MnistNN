d3720dea2c9bbfc3d91b2e8b7a26ac4d5c055477
go36qop <go36qop@mytum.de>
2026-03-18 09:30:12 +0100

    reset

--- src/EarlyStopping.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// Returns true if we should stop training.
-bool early_stopping(const float& val_loss, const int& epoch, const int& patience=20);
--- src/InputLayer.cpp
+++
@@ -1,5 +0,0 @@
-#include "InputLayer.h"
-
-
-InputLayer::InputLayer(const size_t data_size_, const size_t batch_size_)
-:Layer(data_size_, batch_size_){}
